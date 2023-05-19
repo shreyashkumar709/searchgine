e558da10b4dd4ea0ef81a16d28d588aad2a1e766
shreyashkumar709 <119500562+shreyashkumar709@users.noreply.github.com>
2023-05-19 15:08:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+    const searchinput = document.getElementById("search-value");
+    const queryForSearch = '[s]';
+    var appender = "";
+    searchinput.addEventListener("keyup", (e) => {
+        var valueQuery = searchinput.value;
+        if (valueQuery.includes(queryForSearch)) {
+            valueQuery = valueQuery.replace(queryForSearch, "");
+
+            const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(valueQuery)}`;
+
+            window.open(searchUrl, "_blank");
+        }
+    })
+    function updateTime() {
+        const span = document.getElementById('time-time');
+        const currentDate = new Date();
+        const options = {
+            timeZone: 'Asia/Kolkata',
+            hour12: false,
+            hour: '2-digit',
+            minute: '2-digit'
+        };
+        const formattedTime = currentDate.toLocaleString('en-IN', options);
+        span.textContent = formattedTime;
+    }
+
+    // Call the function once to update the time immediately
+    updateTime();
+
+    // Update the time every second
+    setInterval(updateTime, 1000);
+
+})
